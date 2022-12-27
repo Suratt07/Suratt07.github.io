8cf63d0af96af5420401bd72661dfdcbf562e96a
Suratt Suyel <121524177+Suratt07@users.noreply.github.com>
2022-12-28 00:34:19 +0100

    updates

---
+++ js/script.js
@@ -0,0 +1,24 @@
+tippy('#instagram', {
+  content: '1am_anubhab',
+});
+tippy('#steam', {
+  content: 'ANUBH4B',
+});
+tippy('#xbox', {
+  content: 'ANUBH4B',
+});
+tippy('#github', {
+  content: 'ANUBH4B',
+});
+tippy('#discord', {
+  content: 'ANUBH4B#4069',
+});
+tippy('#twitter', {
+  content: 'ANUBH4B',
+});
+tippy('#youtube', {
+  content: 'ANUBH4B PlayZ',
+});
+tippy('#email', {
+  content: 'contact@anubh4b.xyz',
+});
